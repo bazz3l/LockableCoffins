fe0d2d5fe42e82f5f925ed654f6c45b305ee764a
Bazz3l <30571705+bazz3l@users.noreply.github.com>
2020-05-05 13:55:21 +0100

    First commit

---
+++ LockableCoffins.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("Lockable Coffins", "Bazz3l", "1.0.0")]
+    [Description("Allow placement of codelocks on coffins.")]
+    class LockableCoffins : RustPlugin
+    {
+        #region Oxide
+        void OnEntitySpawned(BaseEntity entity)
+        {
+            if (entity is StorageContainer && entity.PrefabName.Contains("coffin"))
+            {
+                MakeLockable(entity as StorageContainer);
+            }
+
+            if (entity is CodeLock)
+            {
+                MoveLock(entity, entity.GetParentEntity());
+            }
+        }
+        #endregion
+
+        #region Core
+        void MakeLockable(StorageContainer container)
+        {
+            container.isLockable = true;
+        }
+
+        void MoveLock(BaseEntity entity, BaseEntity parentEntity)
+        {
+            if (parentEntity != null && parentEntity.PrefabName.Contains("coffin"))
+            {
+                Vector3 currentPos = entity.transform.localPosition;
+                entity.transform.localPosition = currentPos += new Vector3(0.1f, -0.2f, 0f);
+                parentEntity.SendNetworkUpdateImmediate();
+            }
+        }
+        #endregion
+    }
+}
